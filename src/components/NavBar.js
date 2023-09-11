de666d08c33937010442dfce3c6238c3cd06cf84
aaryadevnani <aarya_devnani@hotmail.com>
2023-09-11 01:23:21 -0700

    Changed Nav to MUI nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,63 +1,168 @@
-import React from "react";
-import "./styles/NavBar.css";
+import * as React from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../assets/logo.png";
-function NavBar() {
-  const [click, setClick] = React.useState(false);
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import "./styles/NavBar.css";
 
-  const handleClick = () => setClick(!click);
-  const Close = () => setClick(false);
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography sx={{ my: 2 }}>
+        <div>Morality and Language Lab</div>
+      </Typography>
+      <List>
+        <ListItem key="Publications" disablePadding>
+          <ListItemButton sx={{ textAlign: "center" }}>
+            <ListItemText primary="Publications" sx={{ color: "#000" }} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Login" disablePadding>
+          <ListItemButton sx={{ textAlign: "center" }}>
+            <ListItemText primary="Login" sx={{ color: "#000" }} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Signup" disablePadding>
+          <ListItemButton sx={{ textAlign: "center" }}>
+            <ListItemText primary="Signup" sx={{ color: "#000" }} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <div>
-      <div className={click ? "main-container" : ""} onClick={() => Close()} />
-      <nav className="navbar" onClick={(e) => e.stopPropagation()}>
-        <div className="nav-container">
-          <NavLink exact to="/" className="nav-logo">
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        component="nav"
+        className="toolbar"
+        sx={{ backgroundColor: "#fff", boxShadow: "None", color: "#000" }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              // display: { xs: "block", sm: "block" },
+              fontSize: { lg: "30px", md: "20px", sm: "20px" },
+            }}
+          >
             Morality and Language Lab
-            {/* <img src={logo} alt="logo" width="450px" /> */}
-          </NavLink>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/"
-                activeClassName="active"
-                className="nav-links"
-                onClick={click ? handleClick : null}
+            <Typography
+              component="div"
+              sx={{
+                fontSize: { lg: "20px" },
+                fontWeight: "500",
+                textAlign: "center",
+                display: { xs: "None", sm: "None", md: "block", lg: "block" },
+              }}
+            >
+              University of Southern California
+            </Typography>
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", sm: "flex" },
+              flexDirection: "row",
+              justifyContent: "flex-end",
+            }}
+          >
+            <NavLink to="/" style={{ textDecoration: "none" }}>
+              <Button
+                key="Publications"
+                sx={{
+                  "&:hover": { backgroundColor: "#fff" },
+                  color: "#000",
+                  fontWeight: "1000",
+                }}
               >
                 Publications
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/login"
-                activeClassName="active"
-                className="nav-links"
-                onClick={click ? handleClick : null}
+              </Button>
+            </NavLink>
+            <NavLink to="/login" style={{ textDecoration: "none" }}>
+              <Button
+                key="Login"
+                sx={{
+                  "&:hover": { backgroundColor: "#fff" },
+                  color: "#000",
+                  fontWeight: "1000",
+                }}
               >
                 Login
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/signup"
-                activeClassName="active"
-                className="nav-links"
-                onClick={click ? handleClick : null}
+              </Button>
+            </NavLink>
+            <NavLink to="/signup" style={{ textDecoration: "none" }}>
+              <Button
+                key="Signup"
+                sx={{
+                  "&:hover": { backgroundColor: "#fff" },
+                  color: "#000",
+                  fontWeight: "1000",
+                }}
               >
-                Sign Up
-              </NavLink>
-            </li>
-          </ul>
-          <div className="nav-icon" onClick={handleClick}>
-            <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
-          </div>
-        </div>
+                Signup
+              </Button>
+            </NavLink>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: "100vw",
+              alignContent: "left",
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
       </nav>
-    </div>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+      </Box>
+    </Box>
   );
 }
-export default NavBar;
+
+export default DrawerAppBar;
