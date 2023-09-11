ab69143acf61270de4ba5e26566825e2970179f2
aaryadevnani <aarya_devnani@hotmail.com>
2023-09-11 00:13:58 -0700

    Misc CSS changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import './styles/NavBar.css';
-import { NavLink } from 'react-router-dom';
-
+import React from "react";
+import "./styles/NavBar.css";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
 function NavBar() {
   const [click, setClick] = React.useState(false);
-  
+
   const handleClick = () => setClick(!click);
   const Close = () => setClick(false);
-  
+
   return (
     <div>
-     <div className={click ? "main-container" : ""}  onClick={()=>Close()} />
-      <nav className="navbar" onClick={e => e.stopPropagation()}>
+      <div className={click ? "main-container" : ""} onClick={() => Close()} />
+      <nav className="navbar" onClick={(e) => e.stopPropagation()}>
         <div className="nav-container">
           <NavLink exact to="/" className="nav-logo">
             Morality and Language Lab
+            {/* <img src={logo} alt="logo" width="450px" /> */}
           </NavLink>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
@@ -24,7 +25,7 @@ function NavBar() {
                 activeClassName="active"
                 className="nav-links"
                 onClick={click ? handleClick : null}
-                >
+              >
                 Publications
               </NavLink>
             </li>
@@ -35,7 +36,7 @@ function NavBar() {
                 activeClassName="active"
                 className="nav-links"
                 onClick={click ? handleClick : null}
-                >
+              >
                 Login
               </NavLink>
             </li>
@@ -46,18 +47,17 @@ function NavBar() {
                 activeClassName="active"
                 className="nav-links"
                 onClick={click ? handleClick : null}
-                >
+              >
                 Sign Up
               </NavLink>
             </li>
-          
           </ul>
           <div className="nav-icon" onClick={handleClick}>
             <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
           </div>
         </div>
       </nav>
-    </ div>
+    </div>
   );
 }
-export default NavBar
+export default NavBar;
