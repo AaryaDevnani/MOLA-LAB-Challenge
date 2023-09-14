cc333d3ed091b94cc56b05df2b8ad48e96d3420d
aaryadevnani <aarya_devnani@hotmail.com>
2023-09-14 08:36:12 -0700

    Restructured Project Hierarchy

--- Server/server.js
+++ Server/server.js
@@ -33,7 +33,8 @@ app.use(fileupload());
 app.use(corsMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "..", "Server/build")));
+console.log(path.join(__dirname, "../frontend/build"));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // Route imports
 const userRoute = require("./Routes/Auth");
